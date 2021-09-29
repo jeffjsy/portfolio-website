0d1680c00419707d53c7d3ba3c2d71aed5c53c4f
Jeff Sy <jeffjsy@gmail.com>
2021-09-28 23:53:08 -0400

    Second commit

---
+++ routes/index.js
@@ -0,0 +1,36 @@
+var express = require('express');
+var router = express.Router();
+
+/* GET landing page. */
+router.get('/', function(req, res, next) {
+  res.render('index', { title: 'Home', });
+});
+
+/* GET Home page. */
+router.get('/home', function(req, res, next) {
+  res.render('index', { title: 'Home', });
+});
+
+/* GET About us page. */
+router.get('/about', function(req, res, next) {
+  res.render('index', { title: 'About us', });
+});
+
+/* GET Products page. */
+router.get('/products', function(req, res, next) {
+  res.render('index', { title: 'Products', });
+});
+
+/* GET Services page. */
+router.get('/services', function(req, res, next) {
+  res.render('index', { title: 'Services', });
+});
+
+
+/* GET Contact Us page. */
+router.get('/contact', function(req, res, next) {
+  res.render('index', { title: 'Contact', });
+});
+
+
+module.exports = router;
