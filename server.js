91cebcd90821e4f59b62ed8b7a03ead424a5ef8f
Jeff Sy <jeffjsy@gmail.com>
2021-10-21 22:40:42 -0400

    began with mongodb

--- server.js
+++ server.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 
-var app = require('./app');
+var app = require('./config/app');
 var debug = require('debug')('02-the-express-generator-end:server');
 var http = require('http');
 
