d791722799d39916fea56dbc33e5b2dddbe09663
Jeff Sy <jeffjsy@gmail.com>
2021-10-25 18:37:13 -0400

    update to work with mongodb atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports = 
 {
-  "URI": "mongodb://localhost/contacts"
+  // "URI": "mongodb://localhost/contacts"
+  "URI": "mongodb+srv://Jeffrey:R0CnohV2xpLiLFdV@mongodbsserver.85uuw.mongodb.net/contacts?retryWrites=true&w=majority"
 }
